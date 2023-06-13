ae28811c0eec6ac7382195f92cb57408f69d31c2
Matt <matt328@users.noreply.github.com>
2023-06-13 19:58:35 -0400

    ✨ feat(init): Initial import

---
+++ argocd.tf
@@ -0,0 +1,32 @@
+# this configures required providers
+terraform {
+  required_providers {
+    helm = {
+      source = "hashicorp/helm"
+    }
+  }
+}
+
+# configure a helm provider.  helm provider uses this .kube config file to
+# know which cluster to connect to
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+# for larger tf projects you'd want this in a separate file but it's literally the only one
+# so its just right here
+variable "admin_password" {}
+
+
+# helm is whatever, but the easiest way to install argo is with this helm chart
+# defining the admin_password variable in variables.tf will prompt for its entry on the commandline
+module "argocd" {
+  source         = "aigisuk/argocd/kubernetes"
+  admin_password = var.admin_password
+}
+
+# terraform destroy doesn't work because I don't know why yet, but you can delete the argo namespace with --force
+# update It didn't work because I didn't configure a stable 'state' repository.  TF writes (what it thinks) the current
+# 'state' should be a file instead of querying the system like I thought it would.
---
+++ readme.md
@@ -0,0 +1,9 @@
+This terraform module assumes an already provisioned k8s cluster, and sets up ArgoCD.
+
+Run `terraform init` to download providers and modules.
+
+Run `terraform plan` to preview what it's going to do.
+
+Run `terraform apply` to do the thing.
+
+Check the comments in argocd.tf
